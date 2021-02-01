87ce7eb4117ea4ca1076c8e78db90b2841c7d404
Kushagra Setia <32640056+kushagrasetia@users.noreply.github.com>
2021-02-01 20:18:44 +0530

    Assignment 4 added

---
+++ Core Java 8/Assignment 4/Assignment4Q6.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Assignment4Q6 
+{
+    	public static void main(String[] args) 
+	{
+    	}
+    	public List<String> convertToUpperCase(List<String> list) 
+	{
+    	list.replaceAll(i->i.toUpperCase());
+    	return list;
+    	}  
+}
---
+++ Core Java 8/Assignment 4/Assignment4Q7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Assignment4Q7 {
+    public static void main(String[] args) 
+{
+/* HashMap<String, Integer> map = new HashMap<>();
+convertKeyValueToString(map); */
+}
+    public String convertKeyValueToString(HashMap<String, Integer> map) 
+{
+	StringBuilder str = new StringBuilder();
+	 
+    	for(HashMap.Entry<String,Integer> ele:map.entrySet())
+	 {
+    		str.append(ele.getKey()+ele.getValue());
+    	}
+    	return str.toString();	
+}
+}
