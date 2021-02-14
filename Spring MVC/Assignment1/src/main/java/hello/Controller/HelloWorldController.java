81489256ca08c7b480ec440ab3073849ea111ad3
Kushagra Setia <32640056+kushagrasetia@users.noreply.github.com>
2021-02-14 10:03:20 +0530

    Spring MVC :Q1 Added

---
+++ Spring MVC/Assignment1/src/main/java/hello/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package hello.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/helloWorld.html")
+	public String hello()
+	{
+		return "helloWorld";
+	}
+}
---
+++ Spring MVC/Assignment1/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1><a href="/Assignment1/helloWorld.html">Click On This Link !!!</a></h1>
+</body>
+</html>
